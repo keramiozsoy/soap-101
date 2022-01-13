e34ecb9049bf87ca2f6487999a7d9cb79ad42e4f
kerami <keramiozsoy@hotmail.com>
2022-01-13 22:53:45 +0300

    country-repo-jdk-eleven

---
+++ country-repo-jdk-eleven/src/test/java/com/service/countryrepojdkeleven/CountryRepoJdkElevenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.countryrepojdkeleven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryRepoJdkElevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
