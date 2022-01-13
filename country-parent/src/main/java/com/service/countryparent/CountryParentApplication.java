bfbe9fdf2f3fe06de35fef7a15fdbc0035b88d83
kerami <keramiozsoy@hotmail.com>
2022-01-13 22:56:34 +0300

    country-parent

---
+++ country-parent/src/main/java/com/service/countryparent/CountryParentApplication.java
@@ -0,0 +1,13 @@
+package com.service.countryparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryParentApplication.class, args);
+	}
+
+}
